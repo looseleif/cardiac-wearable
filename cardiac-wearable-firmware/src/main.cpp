1b3846310360e66da5fb304680915564443bd80a
looseleif <and08479@umn.edu>
2023-02-20 23:08:06 -0600

    testing bh sensor

--- cardiac-wearable-firmware/src/main.cpp
+++ cardiac-wearable-firmware/src/main.cpp
@@ -1,40 +1,84 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+/*****************************************************************************
+  BH1790GLC.ino
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  If you want to know what pin the on-board LED is conn
-  ected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
+ Copyright (c) 2016 ROHM Co.,Ltd.
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-#include <Arduino.h>
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+******************************************************************************/
 #include <Wire.h>
-#include <Adafruit_TinyUSB.h> // for Serial
+#include <BH1790GLC.h>
+#include <FlexiTimer2.h>
+#include <stdio.h>
+#include <Arduino.h>
 
+BH1790GLC bh1790glc;
+volatile bool timer_flg;
+char str[100];
 
-// the setup function runs once when you press reset or power the board
+void timer_isr(void);
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(9600);
-  while ( !Serial ) delay(10);   // for nrf52840 with native usb
-  Serial.println("Alive");
-  pinMode(LED_BUILTIN, OUTPUT);
+  byte rc;
+  
+  timer_flg = false;
+  Serial.begin(115200);
+  while (!Serial);
+
+  Wire.begin();
+  Wire.setClock(400000L);
+  
+  rc = bh1790glc.init();
+  if (rc != 0) {
+    Serial.println("BH1790 initialize failed");
+  } else {
+    Serial.println("LEDON Data, LEDOFF Data");
+    FlexiTimer2::stop();
+    FlexiTimer2::set(250, 1.0/8000, timer_isr);  // 32Hz timer
+    FlexiTimer2::start();
+  }
 }
-     
-// the loop function runs over and over again forever
-void loop() {
-  digitalToggle(LED_BUILTIN); // turn the LED on (HIGH is the voltage level)
-  Serial.println("Still alive");
-  delay(1000);                // wait for a second
+
+int main() {
+
+  init();
+  setup();
+
+  while(1){
+
+  byte rc;
+  unsigned short val[2];
+ 
+  if (timer_flg) {
+    rc = bh1790glc.get_val(val);
+    if (rc == 0) {
+      sprintf(str, "%d, %d\n", val[1], val[0]);
+      Serial.print(str);
+    }
+    timer_flg = false;
+  }
+
+  }
+
+  return 0;
+
+}
+
+void timer_isr(void) {
+  timer_flg = true;
 }
