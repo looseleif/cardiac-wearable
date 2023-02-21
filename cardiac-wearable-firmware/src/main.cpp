f3a1f20c3a0c2522d71b370fac8a64894423dd7f
shiro019 <shiro019@umn.edu>
2023-02-20 20:18:35 -0600

    working platformio config

--- cardiac-wearable-firmware/src/main.cpp
+++ cardiac-wearable-firmware/src/main.cpp
@@ -1,9 +1,40 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
+  of use the correct LED pin whatever is the board used.
+  If you want to know what pin the on-board LED is conn
+  ected to on your Arduino model, check
+  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+*/
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_TinyUSB.h> // for Serial
 
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  while ( !Serial ) delay(10);   // for nrf52840 with native usb
+  Serial.println("Alive");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
-
+     
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalToggle(LED_BUILTIN); // turn the LED on (HIGH is the voltage level)
+  Serial.println("Still alive");
+  delay(1000);                // wait for a second
 }
